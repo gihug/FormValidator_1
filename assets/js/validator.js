80141c5906cc5ff8809a04e8979a6b620eee2212
unknown <l2rabcxyz@gmail.com>
2022-01-04 21:09:07 +0700

    done

--- assets/js/validator.js
+++ assets/js/validator.js
@@ -1,5 +1,18 @@
+"use strict";
 function Validator(formSelector) {
   var formRules = {};
+  var _this = this;
+
+  function getParent(element, selector) {
+    while (element.parentElement) {
+      // Nếu tìm thấy thằng selector thì return
+      if (element.parentElement.matches(selector)) {
+        return element.parentElement;
+      }
+      // Gán nó bằng cha nó rồi đi tìm thằng selector
+      element = element.parentElement;
+    }
+  }
   /**
    * Quy ước tạo rule:
    * - Nếu có lỗi thì return `error message`
@@ -30,38 +43,123 @@ function Validator(formSelector) {
   };
   // Lấy ra form element trong DOM theo `formSelector`
   var formElement = document.querySelector(formSelector);
-
   // Chỉ xử lý khi có element trong DOM
   if (formElement) {
     var inputs = formElement.querySelectorAll("[name][rules]");
-
-    for (const input of inputs) {
+    // Lấy từng rule của từng thẻ input
+    for (var input of inputs) {
+      // Thực hiện lấy ra mảng các rule exp:`required|min:6` => [required,min:6]
       var rules = input.getAttribute("rules").split("|");
-      for (const rule of rules) {
+      for (var rule of rules) {
         var ruleInfo;
+        // Thực hiện lấy ra mảng các mảng rule của rule con exp:`min:6` => [min,6]
         var isRuleHasValue = rule.includes(":");
+        // Nếu có gán lại rule = min
         if (isRuleHasValue) {
           ruleInfo = rule.split(":");
           rule = ruleInfo[0];
         }
+        // gán func theo key exp: min => function min()
         var ruleFunc = validatorRules[rule];
+        // Tồn tại mảng rule con exp:`min:6`
         if (isRuleHasValue) {
-          ruleFunc = ruleFunc([1]);
+          // gán func con của func cha những rule có yêu cầu params
+          ruleFunc = ruleFunc(ruleInfo[1]);
+        }
+
+        if (Array.isArray(formRules[input.name])) {
+          formRules[input.name].push(ruleFunc);
+        } else {
+          formRules[input.name] = [ruleFunc];
         }
       }
-
-      if (Array.isArray(formRules[input.name])) {
-        formRules[input.name].push(ruleFunc);
-      } else {
-        formRules[input.name] = [ruleFunc];
-      }
+      // Lắng nghe sự kiện để validate (blur, change, ...)
+      input.onblur = handleValidate;
+      input.oninput = handleClearError;
     }
-
-    // Lắng nghe sự kiện để validate (blur, change, ...)
-
-    input.onblur = handleValidate;
   }
 
   // Hàm thực hiện validate
-  function handleValidate(event) {}
+  function handleValidate(event) {
+    //Trả về func validate
+    var rules = formRules[event.target.name];
+    var errorMessage;
+    rules.some(function (rule) {
+      errorMessage = rule(event.target.value);
+      return errorMessage;
+    });
+
+    if (errorMessage) {
+      var formGroup = getParent(event.target, ".form-group");
+      if (formGroup) {
+        formGroup.classList.add("invalid");
+        var formMessage = formGroup.querySelector(".form-message");
+        if (formMessage) {
+          formMessage.innerText = errorMessage;
+        }
+      }
+    }
+
+    return !errorMessage;
+  }
+
+  // Hàm thực hiện loại bỏ error khi input được nhập
+  function handleClearError(event) {
+    var formGroup = getParent(event.target, ".form-group");
+    if (formGroup.classList.contains("invalid")) {
+      formGroup.classList.remove("invalid");
+      var formMessage = formGroup.querySelector(".form-message");
+      if (formMessage) {
+        formMessage.innerText = "";
+      }
+    }
+  }
+  // Xử lý hành vi submit form
+  formElement.onsubmit = function (event) {
+    event.preventDefault();
+    var inputs = formElement.querySelectorAll("[name][rules]");
+    var isFormValid = false;
+    for (var input of inputs) {
+      if (!handleValidate({ target: input })) {
+        isFormValid = true;
+      }
+    }
+    //Khi không có lỗi
+    if (!isFormValid) {
+      if (typeof _this.onSubmit === "function") {
+        var enableInputs = formElement.querySelectorAll(
+          "[name]:not([disabled])"
+        );
+        var formValues = Array.from(enableInputs).reduce((values, input) => {
+          switch (input.type) {
+            case "radio":
+              values[input.name] =
+                formElement.querySelector(`input[name="${input.name}"]:checked`)
+                  ?.value || "";
+              break;
+            case "checkbox":
+              if (!input.matches(":checked")) {
+                values[input.name] = "";
+                return values;
+              }
+              if (!Array.isArray(values[input.name])) {
+                values[input.name] = [];
+              }
+              values[input.name].push(input.value);
+              break;
+            case "file":
+              values[input.name] = input.files;
+              break;
+            default:
+              values[input.name] = input.value;
+          }
+
+          return values;
+        }, {});
+        _this.onSubmit(formValues);
+      } else {
+        formElement.submit();
+      }
+    }
+  };
 }
